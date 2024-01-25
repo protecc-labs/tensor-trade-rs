a5fb190cc8b53d2e171c659474f79ec7630dfb71
mektigboy <86902415+mektigboy@users.noreply.github.com>
2024-01-25 10:30:19 -0600

    refactor: clean queries scalar

--- src/types/queries_scalar.rs
+++ src/types/queries_scalar.rs
@@ -7,23 +7,13 @@ impl Decimal {
     pub fn new(decimal: String) -> Self {
         Self(decimal)
     }
-
-    // pub fn value(&self) -> &String {
-    //     &self.0
-    // }
 }
 
 impl From<String> for Decimal {
     fn from(item: String) -> Self {
-        Decimal::new(item)
+        Self::new(item)
     }
 }
-
-// impl Into<String> for Decimal {
-//     fn into(self) -> String {
-//         self.0
-//     }
-// }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Timestamp(pub i64);
@@ -32,23 +22,13 @@ impl Timestamp {
     pub fn new(timestamp: i64) -> Self {
         Self(timestamp)
     }
-
-    // pub fn value(&self) -> i64 {
-    //     self.0
-    // }
 }
 
 impl From<i64> for Timestamp {
     fn from(item: i64) -> Self {
-        Timestamp::new(item)
+        Self::new(item)
     }
 }
-
-// impl Into<i64> for Timestamp {
-//     fn into(self) -> i64 {
-//         self.0
-//     }
-// }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct BigInt(pub String);
@@ -61,15 +41,9 @@ impl BigInt {
 
 impl From<String> for BigInt {
     fn from(item: String) -> Self {
-        BigInt::new(item)
+        Self::new(item)
     }
 }
-
-// impl Into<String> for BigInt {
-//     fn into(self) -> String {
-//         self.0
-//     }
-// }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Byte {
@@ -81,20 +55,10 @@ impl Byte {
     pub fn new(data: Vec<u8>) -> Self {
         Self { data }
     }
-
-    // pub fn value(&self) -> &Vec<u8> {
-    //     &self.data
-    // }
 }
 
 impl From<Vec<u8>> for Byte {
     fn from(item: Vec<u8>) -> Self {
-        Byte::new(item)
+        Self::new(item)
     }
 }
-
-// impl Into<Vec<u8>> for Byte {
-//     fn into(self) -> Vec<u8> {
-//         self.data
-//     }
-// }
